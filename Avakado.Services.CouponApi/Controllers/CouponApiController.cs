95733df427fff668dace9052d3754facc7de2c03
PC <PC@DESKTOP-QG8O7R7>
2024-11-26 02:52:08 +0300

    db get işlemleri yapıldı

--- Avakado.Services.CouponApi/Controllers/CouponApiController.cs
+++ Avakado.Services.CouponApi/Controllers/CouponApiController.cs
@@ -1,4 +1,6 @@
 ﻿using Avakado.Services.CouponApi.Data;
+using Avakado.Services.CouponApi.Models;
+using Avakado.Services.CouponApi.Models.Dto;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,9 +11,63 @@ namespace Avakado.Services.CouponApi.Controllers
     public class CouponApiController : ControllerBase
     {
         private readonly AppDbContext _db;
+
+        private readonly ResponseDto _responseDto;
         public CouponApiController(AppDbContext appDbContext )
         {
             _db = appDbContext;
+            _responseDto = new ResponseDto();
+        }
+        [HttpGet]
+        public ResponseDto Get() {
+
+            try
+            {
+                IEnumerable<Coupon> objlist = _db.Coupons.ToList();
+                List<CouponDto> cuponlist = new List<CouponDto>();
+                foreach (var item in objlist)
+                {
+                    var coupon = new CouponDto();
+                    coupon.CouponId = item.Id;  
+                    coupon.CouponCode = item.CouponCode;
+                    coupon.DiscountAmount = item.DiscountAmount;
+                    coupon.MinAmount = item.MinAmount;
+                    cuponlist.Add(coupon);
+                }
+                _responseDto.Result = cuponlist;  
+
+            }
+            catch (Exception ex)
+            {
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = ex.Message;
+            }
+            return _responseDto;
+        }
+
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public ResponseDto  Get(int id)
+        {
+            try
+            {
+                Coupon coupon = _db.Coupons.First(x=> x.Id == id);
+                CouponDto couponDto = new ()
+                {
+                    CouponId = coupon.Id,
+                    CouponCode = coupon.CouponCode,
+                    DiscountAmount = coupon.DiscountAmount,
+                    MinAmount = coupon.MinAmount,
+                };
+                _responseDto.Result = couponDto;
+            }
+            catch (Exception ex)
+            {
+                 _responseDto.IsSuccess=false;
+                _responseDto.Message = ex.Message;
+            }
+            return _responseDto;
         }
     }
 }
